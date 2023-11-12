9c1a13828b8add1513e0b34d316df13ce5995d77
moishy friedman <moshehlf@gmail.com>
2023-11-12 17:55:43 +0200

    update templete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,50 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/home/Home";
+import SignIn from "./components/signIn/SignIn";
+import SignUp from "./components/signUp/SignUp";
+import Product from "./components/product/Product";
+import Category from "./components/category/Category";
+import Compare from "./components/compareProducts/Compare";
+import Cart from "./components/cart/Cart";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signIn",
+    element: <SignIn />,
+  },
+  {
+    path: "/signUp",
+    element: <SignUp />,
+  },
+  {
+    path: "/product",
+    element: <Product />,
+  },
+  {
+    path: "/category",
+    element: <Category />,
+  },
+  {
+    path: "/category/compare",
+    element: <Compare />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
--- src/components/product.tsx
+++
@@ -1,9 +0,0 @@
-
-function Products() {
-  return (
-    <>
-    </>
-  )
-}
-
-export default Products
