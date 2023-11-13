3c6369a4d9658bee446db7b37ae142a35882ae35
moishy friedman <moshehlf@gmail.com>
2023-11-13 15:19:51 +0200

    sign in & sijn up

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,3 +1,41 @@
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
+
 export default function Home() {
-  return <></>;
+  const [categories, setCategories] = useState([]);
+  const [topFive, setTopFive] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const categoriesResult = await axios.get("http://localhost:5080/");
+      setCategories(categoriesResult.data);
+      const topFiveResult = await axios.get("http://localhost:5080/");
+      setTopFive(topFiveResult.data);
+      const productsResult = await axios.get("http://localhost:5080/");
+      setProducts(productsResult.data);
+    }
+    getData();
+  }, []);
+
+  return (
+    <>
+      <div>
+        {categories.map((category) => (
+          <Button>{category}</Button>
+        ))}
+      </div>
+      <div>
+        {topFive.map((category) => (
+          <Button>{category}</Button>
+        ))}
+      </div>
+      <div>
+        {products.map((product) => (
+          <Button>{product}</Button>
+        ))}
+      </div>
+    </>
+  );
 }
--- src/components/signUp/SignUp.tsx
+++ src/components/signUp/SignUp.tsx
@@ -1,3 +1,92 @@
+import { useForm, type FieldValues, UseFormWatch } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { Button, Input, Typography } from "@mui/material";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+
 export default function SignUp() {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const password: UseFormWatch<Text> = watch("password");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const onSubmit = async (data: FieldValues) => {
+    const api = await axios.post("http://localhost:5080/users/signUp", data);
+    if (api.status >= 200 && api.status <= 299) {
+      navigate(location.state.from || "/");
+    } else {
+      alert("try agin");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Typography variant="h5">sign up</Typography>
+      <div>
+        <Input
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: "email must be correct",
+            },
+          })}
+          type="email"
+          placeholder="Enter Email"
+        />
+        {errors.email && <ErrorMessage errors={errors} name="email" render={({ message }) => <p>{message}</p>} />}
+      </div>
+      <div>
+        <Input
+          {...register("password", {
+            required: "password is required",
+            pattern: {
+              value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&()*-+=]).+$/,
+              message: "Password must contain at least one uppercase letter, one lowercase letter, and one special character (@, $, !, %, *, #, (, ), ^, =, +, &)",
+            },
+            minLength: {
+              value: 8,
+              message: "password most be minimum 8 characters",
+            },
+            maxLength: {
+              value: 20,
+              message: "password most be maximum 20 characters",
+            },
+          })}
+          type="password"
+          placeholder="Enter Password"
+        />
+        {errors.password && <ErrorMessage errors={errors} name="password" render={({ message }) => <p>{message}</p>} />}
+      </div>
+      <div>
+        <Input
+          {...register("confirmPassword", {
+            required: "password is required",
+            pattern: {
+              value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&()*-+=]).+$/,
+              message: "Password must contain at least one uppercase letter, one lowercase letter, and one special character (@, $, !, %, *, #, (, ), ^, =, +, &)",
+            },
+            minLength: {
+              value: 8,
+              message: "password most be minimum 8 characters",
+            },
+            maxLength: {
+              value: 20,
+              message: "password most be maximum 20 characters",
+            },
+            validate: (value) => value === password || "The passwords do not match",
+          })}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        {errors.confirmPassword && <ErrorMessage errors={errors} name="confirmPassword" render={({ message }) => <p>{message}</p>} />}
+      </div>
+      <Button type="submit">signUp</Button>
+    </form>
+  );
 }
