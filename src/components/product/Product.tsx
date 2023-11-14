e5a68e397e1aa30def8ce90da60ce0fddf449f9b
Boaz Pash <boazpash123@gmail.com>
2023-11-14 12:02:39 +0200

    working in progress on product card

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -1,3 +1,51 @@
+// import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface IProduct {
+  product_name: string;
+  product_image_url: string;
+  description: string;
+  category_id: string;
+  price: number;
+  stock_quantity: number;
+  view: number;
+}
+
 export default function Product() {
-  return <></>;
+  const [product, setProduct] = useState<IProduct | null>(null);
+  useEffect(() => {
+    const getProduct = async (id:string) => {
+      setProduct(()=>await axios())
+    }
+  }, [])
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small"><AddShoppingCartIcon /></Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
