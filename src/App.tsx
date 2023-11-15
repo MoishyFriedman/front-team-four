6d99176cc5c4d93c5574f1b2695b9a291a206849
moishy friedman <moshehlf@gmail.com>
2023-11-15 22:07:28 +0200

    update home gage and fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ import Product from "./components/product/Product";
 import Category from "./components/category/Category";
 import Compare from "./components/compareProducts/Compare";
 import Cart from "./components/cart/Cart";
+import Header from "./components/Header&bottom";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signIn" element={<SignIn />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/product" element={<Product />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/category/compare" element={<Compare />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route path="/" element={<Header />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/signIn" element={<SignIn />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/category/compare" element={<Compare />} />
+            <Route path="/cart" element={<Cart />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
